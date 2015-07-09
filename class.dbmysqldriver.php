1e06d46b2fde714acb43b8a496da5705f77b5021
Lukasz Filipiak <lukasz.filipiak@yahoo.com>
2015-07-09 01:18:04 +0100

    End of class.dbmysqldriver

--- class.dbmysqldriver.php
+++ class.dbmysqldriver.php
@@ -1,9 +1,48 @@
 <?php
 class DbMySqlDriver
 {
-private static $User="";
-private static $Password="";
-public $dbName;
-mysqli_connect( )
+private $User;
+private $Password;
+private $dbName;
+private $dbHost;
+private $numRows;
+private $handler;
+
+public function __construct($h,$u,$p,$n) {
+    
+    $this->dbHost=$h;
+    $this->User=$u;
+    $this->Password=$p;
+    $this->dbName=$n;
+    
+    $conn=  mysqli_connect($h, $u, $p, $n)
+            or die("-- Failed to connect to Data Server --");
+    $this->handler=$conn;
+  }
+        //END OF __construct
+  
+public function Select($table, $what='*') {
+    
+     $query='SELECT '.$what.' FROM '.$table.';';
+     $result=  mysqli_query($this->handler, $query)
+             or die('-- Failde to recived data result --');
+     $row=array();
+     
+     while($r=mysqli_fetch_assoc($result)){
+         array_push($row, $r);
+     }
+     $num=  mysqli_num_rows($result);
+     $this->numRows=$num;
+     echo $this->numRows;
+     return($row);
+     
 }
+        //END OF Select
+}
+$a=new DbMySqlDriver('localhost','root','','simpleloan');
+$w=$a->Select('users');
+foreach ($w as $value) {
+    echo $value['login'];
+}
+
 ?>
